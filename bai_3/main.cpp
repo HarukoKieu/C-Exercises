b1e7d9f35fedda8403f0715da0edec53310cdba9
HarukoKieu <bethuong426@gmail.com>
2025-09-10 16:22:17 +0700

    Update main.cpp

--- bai_3/main.cpp
+++ bai_3/main.cpp
@@ -1,3 +1,27 @@
+/*
+* Ý chính của thuật toán
+
+– Xây dựng hàm kiểm tra số nguyên tố:
+
+Nếu n ≤ 1 → không phải số nguyên tố.
+
+Nếu n ≤ 3 → là số nguyên tố.
+
+Nếu chia hết cho 2 hoặc 3 → không phải số nguyên tố.
+
+Dùng vòng lặp kiểm tra các số từ 5 đến sqrt(n) với bước nhảy 6 (chỉ kiểm tra i và i + 2, vì mọi số nguyên tố lớn hơn 3 đều có dạng 6k ± 1):.
+
+Nếu chia hết → không phải số nguyên tố.
+
+Ngược lại → là số nguyên tố.
+
+– Dùng unordered_set để lưu các số nguyên tố (tránh trùng lặp).
+
+– Duyệt ma trận, nếu a[i][j] là số nguyên tố thì thêm vào unordered_set.
+
+– Duyệt unordered_set rồi in ra kết quả.    
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
@@ -6,7 +30,7 @@ bool checkPrime(ll n) {
     if (n <= 1) return false;
     if (n <= 3) return true;
     if (n % 2 == 0 || n % 3 == 0) return false;
-    for (ll i = 5; i * i <= n; i += 6) {
+    for (ll i = 5; i <= sqrt(n); i += 6) {
         if (n % i == 0 || n % (i + 2) == 0)
             return false;
     }
@@ -14,7 +38,6 @@ bool checkPrime(ll n) {
 }
 
 int main() {
-
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
@@ -22,21 +45,19 @@ int main() {
     while (T--) {
         int N, M; cin >> N >> M;
         vector<vector<int>> a(N, vector<int>(M));
-        map<int, bool> mp;
+        unordered_set<int> primes;
 
         for (int i = 0; i < N; ++i) {
             for (int j = 0; j < M; ++j) {
                 cin >> a[i][j];
-                if (mp.find(a[i][j]) == mp.end()) {
-                    mp[a[i][j]] = checkPrime(a[i][j]);
+                if (checkPrime(a[i][j])) {
+                    primes.insert(a[i][j]);
                 }
             }
         }
 
-        for (auto it : mp) {
-            if (it.second) { 
-                cout << it.first << " ";
-            }    
+        for (auto x : primes) {
+            cout << x << " ";
         }
         cout << endl;
     }
